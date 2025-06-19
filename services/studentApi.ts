2457aee8a0d767da0a1f94cc7f9639dbe59efb2f
4RCZ1 <marcinowicz1@gmail.com>
2025-06-19 19:04:43 +0200

    feat: INZ-73 add email and phoneNumber fields to StudentDTO and implement addStudent and deleteStudent methods

--- services/studentApi.ts
+++ services/studentApi.ts
@@ -7,6 +7,8 @@ import { ApiClientError, ApiResponse, apiRequest } from "@/services/api";
 
 type StudentDTO = {
   externalId: string;
+  email: string;
+  phoneNumber: string;
   name: string;
   surname: string;
   address: AddressDTO;
@@ -18,6 +20,43 @@ export type StudentType = {
   surname: string;
   address: AddressType;
 };
+
+type StudentRequestDTO = {
+  email: string;
+  phoneNumber: string;
+  name: string;
+  surname: string;
+  address:
+    | { externalId: string }
+    | { addressName: string; addressData: string };
+};
+
+type StudentRequestType = {
+  email: string;
+  phoneNumber: string;
+  name: string;
+  surname: string;
+  address: { id: string } | { name: string; data: string };
+};
+
+function studentRequestConverter(
+  studentRequest: StudentRequestType,
+): StudentRequestDTO {
+  return {
+    email: studentRequest.email,
+    phoneNumber: studentRequest.phoneNumber,
+    name: studentRequest.name,
+    surname: studentRequest.surname,
+    address:
+      "id" in studentRequest.address
+        ? { externalId: studentRequest.address.id }
+        : {
+            addressName: studentRequest.address.name,
+            addressData: studentRequest.address.data,
+          },
+  };
+}
+
 export function studentConverter(studentDTO: StudentDTO): StudentType {
   return {
     id: studentDTO.externalId,
@@ -49,6 +88,39 @@ export const studentApi = {
     } catch (error) {
       if (error instanceof ApiClientError) {
         return { data: null, success: false, message: error.message };
+      }
+      throw error; // Re-throw unexpected errors
+    }
+  },
+
+  async addStudent(studentData: StudentRequestType): Promise<boolean> {
+    try {
+      const response = await apiRequest<boolean>("/auth/register-student", {
+        method: "POST",
+        body: JSON.stringify(studentRequestConverter(studentData)),
+      });
+
+      return response ? true : false;
+    } catch (error) {
+      if (error instanceof ApiClientError) {
+        console.error("Failed to add student:", error.message);
+        return false;
+      }
+      throw error; // Re-throw unexpected errors
+    }
+  },
+
+  async deleteStudent(id: string): Promise<boolean> {
+    try {
+      const response = await apiRequest(`/student/${id}`, {
+        method: "DELETE",
+      });
+
+      return response ? true : false;
+    } catch (error) {
+      if (error instanceof ApiClientError) {
+        console.error("Failed to delete student:", error.message);
+        return false;
       }
       throw error; // Re-throw unexpected errors
     }
